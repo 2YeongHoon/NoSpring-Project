14333c8cfa7ab762fcfc869848935ea420046686
yeonghoon.lee <djaakclsrn12@naver.com>
2022-10-31 20:51:12 +0900

    NEW: 회원 도메인 파일 추가

---
+++ src/main/java/hello/core/member/MemberService.java
@@ -0,0 +1,6 @@
+package hello.core.member;
+
+public interface MemberService {
+    void join(Member member);
+    void findMember(Long memberId);
+}
---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    //TODO: HashMap의 경우 동시성 이슈가 있음으로 ConcurrentHashMap 사용
+    private static Map<Long, Member> store = new HashMap<>();
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(findById(memberId));
+    }
+}
