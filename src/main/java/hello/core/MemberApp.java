00a35a86b326a8a1e9ca2fae35bd49696a7f16dd
yeonghoon.lee <djaakclsrn12@naver.com>
2022-11-06 23:45:18 +0900

    CLN: 스프링 컨테이너로 등록

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,16 +3,22 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 // TEST APP
 public class MemberApp {
 
     public static void main(String[] args){
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class );
 
         Member member1 = new Member(1L, "Lee", Grade.VIP);
         memberService.join(member1);
+
         Member member2 = memberService.findMember(1L);
 
         System.out.println("member1: " + member1.getName());
