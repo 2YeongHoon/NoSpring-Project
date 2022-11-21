374a2d8349d498ccf89c74c07e43e884de18e23b
yeonghoon.lee <djaakclsrn12@naver.com>
2022-11-21 23:35:02 +0900

    IMP: 컴포넌트스캔 추가

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -2,7 +2,9 @@ package hello.core.member;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository{
 
     //TODO: HashMap의 경우 동시성 이슈가 있음으로 ConcurrentHashMap 사용
