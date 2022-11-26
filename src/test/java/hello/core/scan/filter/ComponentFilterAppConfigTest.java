c7bf24c4b8c7c09b72b57a543898f01c7be3c0f5
yeonghoon.lee <djaakclsrn12@naver.com>
2022-11-26 23:52:48 +0900

    NEW: 어노테이션 Filter기능 테스트

---
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,30 @@
+package hello.core.scan.filter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void filterScan(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean("BeanA", BeanA.class);
+        Assertions.assertThat(beanA).isNotNull();
+    }
+
+    @Configuration
+    @ComponentScan(
+        includeFilters = @ComponentScan.Filter(classes = MyIncludeComponent.class),
+        excludeFilters = @ComponentScan.Filter(classes = MyExcludeComponent.class)
+    )
+
+    static class ComponentFilterAppConfig{
+    }
+
+
+
+}
