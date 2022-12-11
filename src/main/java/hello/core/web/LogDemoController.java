646a309ab5d55e7f3ab38eb81de9544c0a54ed28
yeonghoon.lee <djaakclsrn12@naver.com>
2022-12-11 19:32:13 +0900

    NEW: 웹 스코프 주입 방식 변경

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,12 +12,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    // 이대로 사용하면 주입 시점이 달라 에러발생
+//    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest servletRequest){
         String requestUrl = servletRequest.getRequestURL().toString();
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
+
         myLogger.setRequestUrl(requestUrl);
 
         myLogger.log("Controller Test");
